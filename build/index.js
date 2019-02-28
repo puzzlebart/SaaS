5800e2c7e2042d5c18329cbe861dd9247443b89b
Kim Damsleth <kim@damsleth.com>
2019-02-28 22:13:44 +0100

    updated runscripts for heroku

---
+++ build/index.js
@@ -0,0 +1,57 @@
+"use strict";
+
+var _express = _interopRequireDefault(require("express"));
+
+var _morgan = _interopRequireDefault(require("morgan"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// framework
+// request logger
+var app = (0, _express.default)(); // express app 
+
+app.use((0, _morgan.default)('tiny')); // morgan
+// <ENDPOINTS>
+
+app.get('/', function (req, res) {
+  res.send("<h1>SaaS - Simpsons as a Service</h1>");
+}); // default route
+
+var doh = app.get('/doh', function (req, res) {
+  res.json({
+    message: "D'oh!"
+  });
+}); // D'oh!
+
+var caramba = app.get('/caramba', function (req, res) {
+  res.json({
+    message: "¡Ay, caramba!"
+  });
+}); //caramba
+
+var mmm = app.get('/mmm', function (req, res) {
+  res.json({
+    message: "Mmm~mmmmm"
+  });
+}); //mmm
+
+var hi = app.get('/hi', function (req, res) {
+  res.json({
+    message: "Hi-Diddily-Ho!"
+  });
+}); //Hi but in Flanders
+
+var bart = app.get('/characters/bart', function (req, res) {
+  res.json({
+    message: {
+      Name: "Bart Simpson",
+      Bio: "Bartholomew JoJo 'Bart' Simpson (born Feb 23/April 1, 1980)[4] is a main character and the tritagonist of The Simpsons."
+    },
+    Picture: "https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20180319061933"
+  });
+}); // </ENDPOINTS>
+
+app.listen('742', function () {
+  return console.log(".");
+}); // starting server
+//# sourceMappingURL=index.js.map
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const app = express() // express app
 app.use(morgan('tiny')) // morgan
 
 // <ENDPOINTS>
+
 app.get('/', (req, res) => { res.send(`<h1>SaaS - Simpsons as a Service</h1>`)}) // default route
 let doh = app.get('/doh', (req, res) => { res.json({ message: "D'oh!" }) }) // D'oh!
 let caramba = app.get('/caramba', (req, res) => {res.json({message: "¡Ay, caramba!" }) }) //caramba
@@ -13,6 +14,7 @@ let bart = app.get('/characters/bart', (req, res) => {res.json({message: {
 Name:"Bart Simpson",
 Bio:"Bartholomew JoJo 'Bart' Simpson (born Feb 23/April 1, 1980)[4] is a main character and the tritagonist of The Simpsons."},
 Picture:"https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20180319061933"}) })
+
 // </ENDPOINTS>
 
-app.listen('742', ()=> console.log(".")) // starting server
+app.listen('742', ()=> console.log(".")) // starting 
