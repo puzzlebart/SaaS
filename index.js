d2666ccab38eeeb4f79bd6d28276b7a59ed2dc9d
Kim Damsleth <kim@damsleth.com>
2019-02-28 16:41:32 +0100

    bart_dummydata

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ let doh = app.get('/doh', (req, res) => { res.json({ message: "D'oh!" }) }) // D
 let caramba = app.get('/caramba', (req, res) => {res.json({message: "¡Ay, caramba!" }) }) //caramba
 let mmm = app.get('/mmm', (req, res) => {res.json({message: "Mmm~mmmmm" }) }) //mmm
 let hi = app.get('/hi', (req, res) => {res.json({message: "Hi-Diddily-Ho!" }) }) //Hi but in Flanders
+let bart = app.get('/characters/bart', (req, res) => {res.json({message: {
+Name:"Bart Simpson",
+Bio:"Bartholomew JoJo 'Bart' Simpson (born Feb 23/April 1, 1980)[4] is a main character and the tritagonist of The Simpsons."},
+Picture:"https://vignette.wikia.nocookie.net/simpsons/images/6/65/Bart_Simpson.png/revision/latest?cb=20180319061933"}) })
 // </ENDPOINTS>
 
 app.listen('742', ()=> console.log(".")) // starting server
