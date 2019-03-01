60f3ce52d624be1f947b3c5c37265d2ab5cec0ec
Kim Damsleth <kim@damsleth.com>
2019-03-01 18:17:13 +0100

    nicer landing page

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
 const superSecretApiKeys = process.env.APIKEYS.split(",")
 const doEnterpriseLevelSecurityCheck = true;
 
-app.get('/', (req, res) => { res.send(`<h1>SaaS - Simpsons as a Service</h1>`) }) // default route
+
 
 // DaaS
 let doh = app.get('/doh', (req, res) => { res.json({ message: "D'oh!" }) }) // D'oh!
@@ -60,5 +60,80 @@ function EnterpriseLevelSecurityCheck(req, res) {
 
 // Stupid sexy jslint
 const capitalize = (s) => { if (typeof s !== 'string') return ''; return s.charAt(0).toUpperCase() + s.slice(1) }
+
+
+app.get('/', (req, res) => { res.send(`<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Simpsons as a Service</title>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"
+        rel="stylesheet">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:creator" content="@Kimzter">
+    <meta name="og:title" content="Simpsons As A Service">
+    <meta name="og:description" content="SaaS provides a modern, RESTful, scalable API for Simpsons character data.">
+    <script src="//code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="hero-unit">
+            <h1>SaaS</h1>
+            <h2>Simpsons As A Service</h2>
+            <p><em>v1.0.0</em></p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="content" style="margin-left:50px;">
+            <h2 id="introduction">Introduction</h2>
+            <p>SaaS (Simpsons As A Service) provides a modern, RESTful, scalable way of getting Simpsons Character data</p>
+            <h2 id="api">API</h2>
+            <h3 id="contentnegotiation">Content Negotiation</h3>
+            <p>SaaS responds in JSON formatted text</p>
+            <h3 id="operations">Operations</h3>
+            <table class="table" id="ops">
+                <tr>
+                    <th>Path</th>
+                    <th>Description</th>
+                </tr>
+                <tr>
+                    <td>/version</td>
+                    <td>Returns the current SaaS version number.</td>
+                </tr>
+
+                <tr>
+                    <td>/characters</td>
+                    <td>The main character endpoint</td>
+                </tr>
+                <tr>
+                    <td>/doh</td>
+                    <td>D'oh! As A Service</td>
+                </tr>
+                <tr>
+                    <td>/quotes</td>
+                    <td>quotes from a character, should they have any</td>
+                </tr>
+                <tr>
+                    <td>/picture</td>
+                    <td>Returns the main character photo</td>
+                </tr>
+            </table>
+            <h3 id="operations">Example usage</h3>
+            <p><b>cURL</b></p>
+            <code>curl -L "http://saas.puzzlebart.no/characters?Name=Homer%20Simpson" -H apikey:"YOUR_API_KEY"</code>
+            <br/>
+            <br/>
+            <p><b>fetch</b></p>
+            <code>await fetch("http://saas.puzzlebart.no/characters?Name=Homer%20Simpson",{headers:{apikey:"YOUR_API_KEY"}}).then(d=>d.json().then(r=>r))</code>
+            <p></p>
+            <p><a href="https://github.com/puzzlebart/saas">Fork us on github!</a></p>
+            <p>Created by <a href="https://twitter.com/Kimzter">@Kimzter</a></p>
+        </div>
+    </div>
+</body>
+</html>
+`) }) // default route
+
 
 app.listen(process.env.PORT || '3000', () => console.log(`running on port ${process.env.PORT || '3000'}`)) // starting
