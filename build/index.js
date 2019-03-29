2d5e14d46c27fee9918559a6b90c3e17f8e520d3
Kim Damsleth <kim@damsleth.com>
2019-03-29 15:05:44 +0100

    now

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var _saasdata = _interopRequireDefault(require("saasdata.json"));
+var _saasdata = _interopRequireDefault(require("./saasdata.json"));
 
 var _express = _interopRequireDefault(require("express"));
 
@@ -12,7 +12,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); 
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
 function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
